cc3305785e556cadb09c21036666c40b33cf9227
Milena Neves <milena.freire2013@gmail.com>
2023-05-20 12:02:36 -0300

    apagando void number() e private void match(tokentype t)

--- jackcompiler-java-master/src/main/java/br/ufma/ecp/Parser.java
+++ jackcompiler-java-master/src/main/java/br/ufma/ecp/Parser.java
@@ -19,46 +19,12 @@ public class Parser {
     public void parse () {
     }
 
-    void expr() {
-        number();
-        oper();
-    }
-
-    void number () {
-        System.out.println(currentToken.lexeme);
-        match(TokenType.NUMBER);
-    }
 
     private void nextToken () {
         currentToken = peekToken;
         peekToken = scan.nextToken();
     }
 
-   private void match(TokenType t) {
-        if (currentToken.type == t) {
-            nextToken();
-        }else {
-            throw new Error("syntax error");
-        }
-   }
-
-    void oper () {
-        if (currentToken.type == TokenType.PLUS) {
-            match(TokenType.PLUS);
-            number();
-            System.out.println("add");
-            oper();
-        } else if (currentToken.type == TokenType.MINUS) {
-            match(TokenType.MINUS);
-            number();
-            System.out.println("sub");
-            oper();
-        } else if (currentToken.type == TokenType.EOF) {
-            // vazio
-        } else {
-            throw new Error("syntax error");
-        }
-    }
 
     public String VMOutput() {
         return "";
