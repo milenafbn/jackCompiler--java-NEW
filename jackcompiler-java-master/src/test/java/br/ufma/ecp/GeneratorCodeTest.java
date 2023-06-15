4329539fdbaf00c5ca3ddc942ae3c4436c2cbab3
Milena Neves <milena.freire2013@gmail.com>
2023-06-15 17:34:02 -0300

    testes no GeneratorCode
    literais inteiras e operadores

--- jackcompiler-java-master/src/test/java/br/ufma/ecp/GeneratorCodeTest.java
+++ jackcompiler-java-master/src/test/java/br/ufma/ecp/GeneratorCodeTest.java
@@ -1,14 +1,42 @@
 package br.ufma.ecp;
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 import org.junit.Test;
 
 public class GeneratorCodeTest {
+	
     @Test
     public void testInt () {
-	String input = """
-	10
-	""";
-	};
+        var input = """
+            10
+            """;
+        
+        var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+        parser.parseExpression();
+        String actual = parser.VMOutput();
+        String expected = """
+                push constant 10       
+                    """;
+            assertEquals(expected, actual);
+    }
+
+	@Test
+    public void testSimpleExpression () {
+        var input = """
+            10 + 30
+            """;
+        
+        var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+        parser.parseExpression();
+        String actual = parser.VMOutput();
+        String expected = """
+                push constant 10
+                push constant 30
+                add       
+                    """;
+            assertEquals(expected, actual);
+    }
 }
