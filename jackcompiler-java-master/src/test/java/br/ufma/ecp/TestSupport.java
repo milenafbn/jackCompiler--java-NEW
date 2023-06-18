ba7e8352635e831135890796a57c45638f16a812
Milena Neves <milena.freire2013@gmail.com>
2023-06-18 12:15:04 -0300

    erro arquivo correção
    Não corrigiu mas voltou a funcionar

--- jackcompiler-java-master/src/test/java/br/ufma/ecp/TestSupport.java
+++ jackcompiler-java-master/src/test/java/br/ufma/ecp/TestSupport.java
@@ -10,6 +10,4 @@ public class TestSupport {
     public static String fromFile(String path) throws IOException {
         return Files.readString(Paths.get("src/test/resources/"+ path));
     }
-
-    
 }
